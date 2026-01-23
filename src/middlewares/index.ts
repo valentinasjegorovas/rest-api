ba32fedd614b5512724810da0a906a6fe57bd68b
Valentinas <valentinas.jegorovas@gmail.com>
2026-01-23 22:49:25 +0200

    feat: add deleteUser, add updateUser

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -12,7 +12,7 @@ export const isAuthenticated = async (
     const sessionToken = req.cookies["sessionToken"];
 
     if (!sessionToken) {
-      return res.sendStatus(400);
+      return res.sendStatus(403);
     }
 
     const existingUser = await getUserBySessionToken(sessionToken);
@@ -27,5 +27,28 @@ export const isAuthenticated = async (
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
+  }
+};
+
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction,
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "identity._id") as string;
+
+    if (!currentUserId) {
+      return res.sendStatus(403);
+    }
+
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+
+    return next();
+  } catch (error) {
+    console.log(error);
   }
 };
